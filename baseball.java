4b228ac0fc0e43e85b05c077757cbdde702bfb0a
Supagal <lgrif57@gmail.com>
2018-09-18 16:45:44 -0400

    Add files via upload

---
+++ baseball.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+public class baseball {
+	
+public static void main(String [] args)
+	Scanner sc = new Scanner(System.in);
+
+	System.out.println("Enter Player Name: ");
+	String playerName= sc.nextLine();
+	
+	System.out.println("Enter Hits: ");
+	double hits = sc.nextDouble();
+	
+	
+	System.out.println("Enter doubles: ");
+	double Doubles = sc.nextDouble();
+	
+	
+	System.out.println("Enter triples: ");
+	double Triple = sc.nextDouble();
+	
+	
+	System.out.println("Enter home runs: ");
+	double homeRuns=sc.nextDouble();
+	
+	
+	System.out.println("Enter runs: ");
+	double runs = sc.nextDouble();
+	
+	
+	System.out.println("Enter walks:");
+	double walks = sc.nextDouble();
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+
